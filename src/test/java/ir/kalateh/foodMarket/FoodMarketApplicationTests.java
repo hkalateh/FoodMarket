db4d8de9828248bc2da1a6f700c87834ac8529a7
hossein <h.kalateh72@gmail.com>
2020-05-14 18:09:46 +0430

    add welcome page

--- src/test/java/ir/kalateh/foodMarket/FoodMarketApplicationTests.java
+++ src/test/java/ir/kalateh/foodMarket/FoodMarketApplicationTests.java
@@ -1,13 +1,32 @@
 package ir.kalateh.foodMarket;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import ir.kalateh.foodMarket.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class FoodMarketApplicationTests {
-
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)
+public class FoodMarketApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
-	void contextLoads() {
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(
+						containsString("Welcome to...")));
 	}
 
 }
